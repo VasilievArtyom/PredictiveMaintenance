9fc888452ab5c2dd091180d4acd465af68947ca8
VasilievAtyom <artyom.vasiliev.1@gmail.com>
2020-03-15 03:53:16 +0300

    extra stat plots

--- private/Vasiliev/plot_stat.py
+++ private/Vasiliev/plot_stat.py
@@ -21,16 +21,16 @@ B_n_labels = [r'$B_1$', r'$B_2$', r'$B_3$', r'$B_4$', r'$B_5$', r'$B_6$', r'$B_7
 
 
 failure = np.sum(np.invert(S).astype(int), axis=0)
+failure_over_time = np.sum(np.invert(S).astype(int), axis=1)
 
 
 # Draw acf plots
 outpath = "../../plots/stat"
 
-fig, ax = plt.subplots(figsize=(10, 5))
+fig, ax = plt.subplots(figsize=(10, 3))
 ax.bar(np.arange(10), failure, color='crimson')
 plt.grid(which='both', axis='y')
 plt.ylabel(r'Failures per block count in time samples')
-
 ax.set_xticks(np.arange(len(B_n_labels)))
 ax.set_xticklabels(B_n_labels)
 plt.tight_layout()
@@ -38,3 +38,31 @@ plt.draw()
 fig.savefig(path.join(outpath, "FailurePerBlock.png"))
 plt.clf()
 
+fr=0
+fig, ax = plt.subplots(figsize=(5, 5))
+# ax.plot(N[-100:,0], Mode[-100:], marker='x', ls='-', color='m', label='Mode')
+ax.bar(N[fr:,0], failure_over_time[fr:], color='crimson', label='failure count')
+plt.grid()
+# plt.ylabel(r'$|\ddot{T^{(max)}}|$')
+plt.xlabel(r'$n$')
+ax.xaxis.grid(b=True, which='both')
+ax.yaxis.grid(b=True, which='major')
+ax.legend(loc='best', frameon=True)
+plt.tight_layout()
+plt.draw()
+fig.savefig(path.join(outpath, "failure_over_time.png"))
+plt.clf()
+
+fig, ax = plt.subplots(figsize=(5, 5))
+ax.bar(N[fr:,0], Mode[fr:].astype(int), color='m', label='Mode')
+# ax.plot(N[-100:,0], failure_over_time[-100:], marker='.', ls='-', color='crimson', label='failure count')
+plt.grid()
+# plt.ylabel(r'$|\ddot{T^{(max)}}|$')
+plt.xlabel(r'$n$')
+ax.xaxis.grid(b=True, which='both')
+ax.yaxis.grid(b=True, which='major')
+ax.legend(loc='best', frameon=True)
+plt.tight_layout()
+plt.draw()
+fig.savefig(path.join(outpath, "mode_over_time.png"))
+plt.clf()
